365c7b997c20f00ee58b0ef18ebb08373e0eda84
Alana le Feber <alana.l.f@upcmail.nl>
2024-09-18 14:21:22 +0200

    gegevens inladen

--- connection.php
+++ connection.php
@@ -1,17 +1,4 @@
 <?php
-//// connection.php
-//$host = 'localhost';
-//$dbname = 'tle1_database';
-//$user = 'root';
-//$pass = '';
-//
-//// Create the connection
-//$conn = mysqli_connect($host, $user, $pass, $dbname);
-//
-//// Check the connection
-//if (!$conn) {
-//    die("Connection failed: " . mysqli_connect_error());
-//}
 
 $host = "localhost";
 $user = "root";
@@ -19,8 +6,8 @@ $password = "";
 $database = "tle1_database";
 
 $db = mysqli_connect($host, $user, $password, $database)
-or die("Error: " . mysqli_connect_error());;
+or die("Error: " . mysqli_connect_error());
 
-//?>
+?>
 
 
