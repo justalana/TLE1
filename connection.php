197ca4c308085b934e9a0450374b17d2cf9a180d
Alana le Feber <alana.l.f@upcmail.nl>
2024-09-18 13:57:16 +0200

    gegevens inladen

--- connection.php
+++ connection.php
@@ -1,15 +1,26 @@
 <?php
-// connection.php
-$host = 'localhost';
-$dbname = 'TLE1_database';
-$user = 'root';
-$pass = '';
+//// connection.php
+//$host = 'localhost';
+//$dbname = 'tle1_database';
+//$user = 'root';
+//$pass = '';
+//
+//// Create the connection
+//$conn = mysqli_connect($host, $user, $pass, $dbname);
+//
+//// Check the connection
+//if (!$conn) {
+//    die("Connection failed: " . mysqli_connect_error());
+//}
 
-// Create the connection
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "tle1_database";
 
-// Check the connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
+$db = mysqli_connect($host, $user, $password, $database)
+or die("Error: " . mysqli_connect_error());;
+
+//?>
+
+
---
+++ profile.php
@@ -0,0 +1,40 @@
+<?php
+/** @var mysqli $db */
+require_once 'connection.php';
+
+// Retrieve user information based on the user ID stored in the session
+$query = 'SELECT * FROM users WHERE ID = 1';
+$result = mysqli_query($db, $query)
+or die('Error ' . mysqli_error($db) . ' with query ' . $query);
+
+//save book details in array
+$user_info = mysqli_fetch_assoc($result);
+
+mysqli_close($db);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Profile</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+</head>
+
+<body>
+<div>
+    <h2><?= htmlentities($user_info['name'])?></h2>
+    <section>
+        <ul>
+            <li>Email: <?= htmlentities($user_info['email'])?></li>
+            <li>Phone number: 0<?= htmlentities($user_info['phone_number'])?></li>
+            <li>Date of birth: <?= htmlentities($user_info['date_of_birth'])?></li>
+            <li>Height: <?= htmlentities($user_info['height'])?></li>
+            <li>Weight: <?= htmlentities($user_info['weight'])?></li>
+            <li>BMI: <?= htmlentities($user_info['bmi'])?></li>
+            <li>Blood Type: <?= htmlentities($user_info['blood_type'])?></li>
+        </ul>
+    </section>
+</div>
+</body>
+</html>
