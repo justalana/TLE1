2ca7700cd072314f93bed0552e77d40c4428a3c2
roshanmomand <1028086@hr.nl>
2024-09-18 12:33:58 +0200

    ..

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $users = $classLoadTest->getUsers();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="css/index.css">
-    <script src="js/index.js" defer></script>
+<!--    <script src="js/index.js" defer></script>-->
     <title>Vitalis System Homepage</title>
 </head>
 <body>
