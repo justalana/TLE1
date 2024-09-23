6e2902bab221edb34822907847a4c451e11900a1
Alana le Feber <alana.l.f@upcmail.nl>
2024-09-23 14:20:11 +0200

    idk

--- profile.php
+++ profile.php
@@ -24,25 +24,34 @@ mysqli_close($db);
     <p>navbar</p>
 </nav>
 <body>
-<div class="flex-container">
-    <div class="flex-box">
-        <h2 id="user_name"><?= htmlentities($user_info['name'])?></h2>
+<div class="user-container">
+    <div class="user-box">
+        <h1 id="user_name"><?= htmlentities($user_info['name'])?></h1>
         <div>
             <p class="category">Email</p><p><?= htmlentities($user_info['email'])?></p>
             <p class="category">Telefoonnummer</p><p>0<?= htmlentities($user_info['phone_number'])?></p>
             <p class="category">Geboortedatum</p><p><?= htmlentities($user_info['date_of_birth'])?></p>
-            <p class="category">Lengte</p><p><?= htmlentities($user_info['height'])?></p>
-            <p class="category">Gewicht</p><p><?= htmlentities($user_info['weight'])?></p>
+            <p class="category">Lengte</p><p><?= htmlentities($user_info['height'])?> cm</p>
+            <p class="category">Gewicht</p><p><?= htmlentities($user_info['weight'])?> kg</p>
             <p class="category">BMI</p><p><?= htmlentities($user_info['bmi'])?></p>
             <p class="category">Bloedtype</p><p><?= htmlentities($user_info['blood_type'])?></p>
         </div>
     </div>
-    <div class="flex-box" id="image_box">
+    <div class="user-box" id="image_box">
         <img id="user_profile" src="img/user_prof.png" alt="profile image">
         <h2>Beginnende Tester</h2>
         <img src="img/medal.png" alt="medal image">
         <p id="debt">-€100.000,-</p>
         <button id="edit_prof">Profiel Bewerken</button>
+    </div>
+</div>
+<div>
+    <h1>Voltooide onderzoeken</h1>
+    <div class="exp-container">
+        <div class="exp-box">
+
+        </div>
+
     </div>
 </div>
 </body>
